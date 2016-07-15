5f760ea11cd600cfc15e074d504062f45e80aeec
Blaž Oražem <blaz.orazem@spletna-postaja.com>
2016-07-15 17:40:29 +0200

    Ep 5 - Channels

--- app/CommunityLink.php
+++ app/CommunityLink.php
@@ -7,15 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class CommunityLink extends Model
 {
     protected $fillable = [
-        'channel_id',
-        'title',
-        'link',
+        'channel_id', 'title', 'link'
     ];
-
-    public function creator()
-    {
-        return $this->belongsTo(User::class, 'user_id');
-    }
 
     public static function from(User $user)
     {
@@ -23,14 +16,22 @@ class CommunityLink extends Model
 
         $link->user_id = $user->id;
 
-        // TEMPORARY
-        $link->channel_id = 1;
-
         return $link;
     }
 
     public function contribute($attributes)
     {
         return $this->fill($attributes)->save();
+    }
+
+
+    public function creator()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function channel()
+    {
+        return $this->belongsTo(Channel::class);
     }
 }
--- resources/views/community/add-link.blade.php
+++ resources/views/community/add-link.blade.php
@@ -1,24 +1,58 @@
-<div class="col-md-4">
-    <h3>Contribute a Link</h3>
+@if (Auth::check())
+    <div class="col-md-4">
+        <h3>Contribute a Link</h3>
 
-    <div class="panel panel-default">
-        <div class="panel-body">
-            <form method="POST" action="/community"  role="form">
-                {{ csrf_field() }}
-                <div class="form-group {{ $errors->has('title') ? 'has-error' : '' }}">
-                    <label for="title">Title</label>
-                    <input type="text" class="form-control" name="title" id="title" placeholder="What is the title of your article?" required>
-                    {!! $errors->first('title', '<span class="help-block">:message</span>') !!}
-                </div>
-                <div class="form-group {{ $errors->has('link') ? 'has-error' : '' }}">
-                    <label for="link">Link</label>
-                    <input type="text" class="form-control" name="link" id="title" placeholder="What is the URL?" required>
-                    {!! $errors->first('link', '<span class="help-block">:message</span>') !!}
-                </div>
-                <div class="form-group">
-                    <button type="submit" class="btn btn-primary">Contribute Link</button>
-                </div>
-            </form>
+        <div class="panel panel-default">
+            <div class="panel-body">
+                <form method="POST" action="/community"  role="form">
+                    {{ csrf_field() }}
+
+                    <div class="form-group {{ $errors->has('channel_id') ? 'has-error' : '' }}">
+                        <label for="channel">Channel:</label>
+                        <select name="channel_id" id="channel" class="form-control">
+                            <option selected disabled>Pick a Channel...</option>
+
+                            @foreach($channels as $channel)
+                                <option value="{{ $channel->id }}" {{ old('channel_id') == $channel->id ? 'selected' : '' }}>
+                                    {{ $channel->title }}
+                                </option>
+                            @endforeach
+                        </select>
+
+                        {!! $errors->first('channel_id', '<span class="help-block">:message</span>') !!}
+                    </div>
+
+                    <div class="form-group {{ $errors->has('title') ? 'has-error' : '' }}">
+                        <label for="title">Title:</label>
+                        <input type="text"
+                               class="form-control"
+                               name="title"
+                               id="title"
+                               placeholder="What is the title of your article?"
+                               value="{{ old('title') }}"
+                               required>
+
+                        {!! $errors->first('title', '<span class="help-block">:message</span>') !!}
+                    </div>
+
+                    <div class="form-group {{ $errors->has('link') ? 'has-error' : '' }}">
+                        <label for="link">Link:</label>
+                        <input type="text"
+                               class="form-control"
+                               name="link"
+                               id="title"
+                               placeholder="What is the URL?"
+                               value="{{ old('link') }}"
+                               required>
+
+                        {!! $errors->first('link', '<span class="help-block">:message</span>') !!}
+                    </div>
+
+                    <div class="form-group">
+                        <button type="submit" class="btn btn-primary">Contribute Link</button>
+                    </div>
+                </form>
+            </div>
         </div>
     </div>
-</div>
+@endif
