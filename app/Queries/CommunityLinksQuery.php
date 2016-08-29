7f737be3aea7c09a91a71e7936711ea2c9249808
Blaž Oražem <blaz@orazem.info>
2016-08-29 21:30:19 +0200

    Ep 13 - Most Recent or Most Popular

--- app/Queries/CommunityLinksQuery.php
+++ app/Queries/CommunityLinksQuery.php
@@ -8,16 +8,12 @@ class CommunityLinksQuery
 {
     public function get($sortByPopular, $channel)
     {
-        $orderBy = $sortByPopular ? 'vote_count' : 'updated_at';
+        $orderBy = $sortByPopular ? 'votes_count' : 'updated_at';
 
-        return CommunityLink::with('votes', 'creator', 'channel')
+        return CommunityLink::with('creator', 'channel')
+            ->withCount('votes')
             ->forChannel($channel)
             ->where('approved', 1)
-            ->leftJoin('community_links_votes', 'community_links_votes.community_link_id', '=', 'community_links.id')
-            ->selectRaw(
-                'community_links.*, count(community_links_votes.id) as vote_count'
-            )
-            ->groupBy('community_links.id')
             ->orderBy($orderBy, 'DESC')
             ->paginate(3);
     }
